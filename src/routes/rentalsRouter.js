1627d7fdbc37f099a78ee645de50015d8df5a688
starunz <busouza01@gmail.com>
2022-03-06 21:28:46 -0500

    daysRented validation

--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 import { postRentals } from "../controllers/rentalsController.js";
 
+import rentalValidate from "../middlewares/validateRental.js";
+
 const rentalsRouter = Router();
 
-rentalsRouter.post('/rentals', postRentals);
+rentalsRouter.post('/rentals',rentalValidate, postRentals);
 
 export default rentalsRouter;
